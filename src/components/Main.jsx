c0072744ee54eda8270aa3c6112a1bc644cffc83
Bibek Regmi <1016matrixbibek@gmail.com>
2023-09-25 21:47:07 +0545

    Styling main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,81 @@
-import React from 'react'
+import React from "react";
 import { BiArrowBack } from "react-icons/bi";
-import {BiSolidUserCircle} from "react-icons/bi"
+import { AiOutlineMail } from "react-icons/ai";
+import { CiLocationOn } from "react-icons/ci";
+import { PiTwitterLogoLight } from "react-icons/pi";
+import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
+import { MdDateRange } from "react-icons/md";
+import {BsGlobe} from "react-icons/bs"
 
 const Main = () => {
   return (
     <>
-    <div className='top'>
-        <a href="/"><h1><BiArrowBack/></h1></a>
-        
+      <div className="top">
+        <a href="/">
+          <h1>
+            <BiArrowBack />
+          </h1>
+        </a>
+
         <button>Hire Me</button>
+      </div>
 
-       
-    </div>
-    
-  
-    
-    <div className='container'>
-            
-    </div>
+      <img className="image" src="/image.png" alt="" />
+
+      <div className="main-container">
+        <div className="nameSection">
+          <h1>John Carter</h1>
+          <h4>@username</h4>
+        </div>
+
+        <div className="detailsSection">
+          <div className="sectionLeft">
+            <h1>20</h1>
+            <p>Followers</p>
+          </div>
+          <div className="sectionMiddle">
+            <h1>4.5K</h1>
+            <p>Following</p>
+          </div>
+          <div className="sectionRight">
+            <h1>16</h1>
+            <p>Repositories</p>
+          </div>
+        </div>
+
+        <div className="bottom-box">
+          <div className="cardLeft">
+            <div className="leftPart">
+              <AiOutlineMail /> <span>Email</span>
+              <h4>xyz@gmail.com</h4>
+
+              <CiLocationOn /> <span>location</span>
+              <h4>New York</h4>
+
+              <PiTwitterLogoLight /><span>Twitter</span>
+              <h4>@john-4</h4>
+            </div>
+
+
+            <div className="rightPart">
+              <HiOutlineBuildingOffice2 /> <span>Organization</span>
+              <h4>Github Inc.</h4>
+
+              <MdDateRange/> <span>Joined Date</span>
+              <h4>23 Mar, 2022</h4>
+
+              <BsGlobe/><span>Website</span>
+              <h4>github.com</h4>
+            </div>
+          </div>
+          <div className="cardRight">
+            <h4>Bio</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ipsum laudantium eveniet facere itaque odit, commodi modi dolore unde vero.</p>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
